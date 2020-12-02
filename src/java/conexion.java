d5473ed698f63525d4979b41e9fd56a03d076e5a
ManuVazEpik <manuvaz2003@gmail.com>
2020-12-02 14:53:38 -0600

    Creada la clase conexion

---
+++ src/java/conexion.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.sql.*;
+
+/**
+ *
+ * @author manua
+ */
+public class conexion {
+    
+    public static Connection getConexion(){
+    
+        Connection con = null;
+        String url, username, password;
+        url = "jdbc:mysql://localhost:3306/base?serverTimezone=UTC";
+        username = "root";
+        password = "n0m3l0";
+        
+        try{
+        
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection(url,username,password);
+            System.out.println("Se conectó uwu");
+            
+        }catch(Exception e){
+            System.out.println("No se conectó unu");
+            System.out.println(e.getMessage());
+            System.out.println(e.getStackTrace());
+        }
+        
+        
+        return con;
+    }
+    
+    
+    
+}
