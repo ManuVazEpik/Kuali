0060ec3667ae979ec248ba052f1ee4090e4ab6f2
ManuVazEpik <manuvaz2003@gmail.com>
2020-12-17 02:09:57 -0600

    Ivan: Avanzando en la clase cifrar, metodo descifrarAES ya esta casi terminado

--- src/java/cifrar.java
+++ src/java/cifrar.java
@@ -69,11 +69,47 @@ public class cifrar {
         return usuario;
     }
     
-    usuario AESDescifrar(String nombre, String apellido, int edad, String telefono){
+    ArrayList AESDescifrar(String nombre, String apellido, String edad, String telefono){
     
-        usuario u = new usuario();
+        ArrayList u = new ArrayList();
+        u.clear();
         String llaveS = "AxolosoftwareWin";
         
+        SecretKeySpec key = new SecretKeySpec(llaveS.getBytes(), "AES");
+        
+        Cipher cifrado;
+        
+        try{
+            
+            System.out.println("Verificamos el llenado de datos");
+            System.out.println(nombre);
+            System.out.println(apellido);
+            System.out.println(edad);
+            System.out.println(telefono);
+            System.out.println("***********************************");
+            
+            cifrado = Cipher.getInstance("AES");
+            System.out.println("Creado cifrado con AES");
+            cifrado.init(Cipher.DECRYPT_MODE, key);
+            System.out.println("Iniciamos cifrado en modo decrypt");
+            
+            byte nombreCifrado[] = cifrado.doFinal(nombre.getBytes());
+            byte apellidoCifrado[] = cifrado.doFinal(apellido.getBytes());
+            byte edadCifrado[] = cifrado.doFinal(edad.getBytes());
+            byte telefonoCifrado[] = cifrado.doFinal(telefono.getBytes());
+            
+            System.out.println("Verificamos que se descifró correctamente");
+            System.out.println(nombreCifrado);
+            System.out.println(apellidoCifrado);
+            System.out.println(edadCifrado);
+            System.out.println(telefonoCifrado);
+            
+            u.add(nombreCifrado);
+            u.add(apellidoCifrado);
+            u.add(edadCifrado);
+            u.add(telefonoCifrado);
+            
+        }catch(Exception e){}
         
         
         return u;
@@ -83,6 +119,7 @@ public class cifrar {
     void DESCifrar(){
     
         String llaveS = "AxolosoftwareWin";
+        
         
         
     }
