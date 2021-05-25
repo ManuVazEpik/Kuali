5c211b14ca46b119b117500b3f130d8c530a2530
EmilianoRivasMX <emilianorivasxd@gmail.com>
2021-05-25 02:08:11 -0500

    FAQ terminado, solo falta las caras del buzon

--- web/js/preguntasFrecuentes.js
+++ web/js/preguntasFrecuentes.js
@@ -1,38 +1,9 @@
-const preguntas = document.querySelectorAll('button');
+// Traemos todas las preguntas con la clase .contenedor-pregunta
+const preguntas = document.querySelectorAll('.preguntas .contenedor-pregunta');
 
-function desplegarPregunta() {
-    const respuestas = document.querySelectorAll('.respuesta');
-
-    for (i = 0; i < respuestas.length; i++) {
-        const estadoPregunta = respuestas[i].getAttribute('respuesta-desplegada');
-      
-        if (estadoPregunta == 'false') {
-          respuesta[i].setAttribute('respuesta-desplegada', 'true');
-          
-          //Cambiar el icono de la pregunta      
-        }
-        else if(estadoPregunta == 'true') {
-          respuestas[i].setAttribute('respuesta-desplegada', 'false');
-        }
-        else
-          console.log('Valio barriga Sr verga xd');
-    }
-}
-
-preguntas.forEach(pregunta => pregunta.addEventListener('click', desplegarPregunta));
-
-const items = document.querySelectorAll(".accordion button");
-
-function toggleAccordion() {
-  const itemToggle = this.getAttribute('aria-expanded');
-  
-  for (i = 0; i < items.length; i++) {
-    items[i].setAttribute('aria-expanded', 'false');
-  }
-  
-  if (itemToggle == 'false') {
-    this.setAttribute('aria-expanded', 'true');
-  }
-}
-
-items.forEach(item => item.addEventListener('click', toggleAccordion));
+// Agregamos un EventListener por cada pregunta que tenemos
+preguntas.forEach((pregunta) => {
+    pregunta.addEventListener('click', (e) => {
+        e.currentTarget.classList.toggle('activa');
+    });
+});
