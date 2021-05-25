c49e6cf894748604f73e3f04d18677af9c7e228f
EmilianoRivasMX <emilianorivasxd@gmail.com>
2021-05-24 22:26:30 -0500

    Quejas (falta funconalidad de experiencia)

--- web/js/preguntasFrecuentes.js
+++ web/js/preguntasFrecuentes.js
@@ -1,18 +1,38 @@
 const preguntas = document.querySelectorAll('button');
 
 function desplegarPregunta() {
-    const estadoPregunta = this.getAttribute('respuesta-desplegada');
+    const respuestas = document.querySelectorAll('.respuesta');
 
-    for (i = 0; i < preguntas.length; i++) {
-        preguntas[i].setAttribute('respuesta-desplegada', 'false');
-      }
-
-    if (estadoPregunta == 'false') {
-    this.setAttribute('respuesta-desplegada', 'true');
-    
-    //Cambiar el icono de la pregunta
-
+    for (i = 0; i < respuestas.length; i++) {
+        const estadoPregunta = respuestas[i].getAttribute('respuesta-desplegada');
+      
+        if (estadoPregunta == 'false') {
+          respuesta[i].setAttribute('respuesta-desplegada', 'true');
+          
+          //Cambiar el icono de la pregunta      
+        }
+        else if(estadoPregunta == 'true') {
+          respuestas[i].setAttribute('respuesta-desplegada', 'false');
+        }
+        else
+          console.log('Valio barriga Sr verga xd');
     }
 }
 
 preguntas.forEach(pregunta => pregunta.addEventListener('click', desplegarPregunta));
+
+const items = document.querySelectorAll(".accordion button");
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute('aria-expanded');
+  
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute('aria-expanded', 'false');
+  }
+  
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
