0eac2c1e646d1fd72abce1edd23f4d3b8057137d
Samuel-Sierra <bocalin@hotmail.com>
2021-06-07 14:27:57 -0500

    Correccion de error del buzon de quejas

--- web/js/satisfaccion.js
+++ web/js/satisfaccion.js
@@ -4,47 +4,26 @@
  * and open the template in the editor.
  */
 
-// Ciculos al seleccionar una cara
-let cara1 = document.getElementById('cara1');
-let cara2 = document.getElementById('cara2');
-let cara3 = document.getElementById('cara3');
 
-cara1.onclick = function() {
-    cara1.classList.add('select');
-    cara2.classList.remove('select');
-    cara3.classList.remove('select');
-};
-
-cara2.onclick = function() {
-    cara2.classList.add('select');
-    cara1.classList.remove('select');
-    cara3.classList.remove('select');
-};
-
-cara3.onclick = function() {
-    cara3.classList.add('select');
-    cara2.classList.remove('select');
-    cara1.classList.remove('select');
-};
 
 // Encuesta de satisfacción
 
 function satisfaccion1(){
     
-    document.querySelector('.caras').innerHTML = '<input type="hidden" value="3" name="satisfaccion"/><button onclick="return satisfaccion1()"><i class="far fa-grin-beam fa-10x"></i></button><button onclick="return satisfaccion2()"><i class="far fa-meh fa-10x"></i></button><button onclick="return satisfaccion3()"><i class="far fa-angry fa-10x"></i></button>';
+    document.querySelector('.caras').innerHTML = '<input type="hidden" value="3" name="satisfaccion"/><button onclick="return satisfaccion1()" class="select"><i class="far fa-grin-beam fa-10x"></i></button><button onclick="return satisfaccion2()"><i class="far fa-meh fa-10x"></i></button><button onclick="return satisfaccion3()"><i class="far fa-angry fa-10x"></i></button>';
     
     return false;
 }
 
 function satisfaccion2(){
     
-    document.querySelector('.caras').innerHTML = '<input type="hidden" value="2" name="satisfaccion"/><button onclick="return satisfaccion1()"><i class="far fa-grin-beam fa-10x"></i></button><button onclick="return satisfaccion2()"><i class="far fa-meh fa-10x"></i></button><button onclick="return satisfaccion3()"><i class="far fa-angry fa-10x"></i></button>';
+    document.querySelector('.caras').innerHTML = '<input type="hidden" value="2" name="satisfaccion"/><button onclick="return satisfaccion1()"><i class="far fa-grin-beam fa-10x"></i></button><button onclick="return satisfaccion2()" class="select"><i class="far fa-meh fa-10x"></i></button><button onclick="return satisfaccion3()"><i class="far fa-angry fa-10x"></i></button>';
     return false;
 }
 
 function satisfaccion3(){
     
-    document.querySelector('.caras').innerHTML = '<input type="hidden" value="1" name="satisfaccion"/><button onclick="return satisfaccion1()"><i class="far fa-grin-beam fa-10x"></i></button><button onclick="return satisfaccion2()"><i class="far fa-meh fa-10x"></i></button><button onclick="return satisfaccion3()"><i class="far fa-angry fa-10x"></i></button>';
+    document.querySelector('.caras').innerHTML = '<input type="hidden" value="1" name="satisfaccion"/><button onclick="return satisfaccion1()"><i class="far fa-grin-beam fa-10x"></i></button><button onclick="return satisfaccion2()"><i class="far fa-meh fa-10x"></i></button><button onclick="return satisfaccion3()" class="select"><i class="far fa-angry fa-10x"></i></button>';
     return false;
 }
 
